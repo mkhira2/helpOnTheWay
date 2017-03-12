9e978f2459fb093d50eb5c94bc1d4be25625cf06
james sewell <jamesjosephsewell@gmail.com>
2017-03-11 23:33:57 -0600

    lol

--- src/scripts/views/components/formComponents/userName.js
+++ src/scripts/views/components/formComponents/userName.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Backone from 'backbone'
 
 const UserNameComponent = React.createClass (){
-	render: {
+	render: function() {
 		<div className="form-group">
 			<input placeholder="User Name" className="form-control" />
 		</div>
