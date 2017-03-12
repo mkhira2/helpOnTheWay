eee5e2b3a8381a28d97d5ebc4a69c4bcf8db326d
Kenji Hirabayashi <mkhira2@gmail.com>
2017-03-12 12:49:23 -0500

    added placeholder headline

--- src/scripts/views/headerComponent.js
+++ src/scripts/views/headerComponent.js
@@ -5,7 +5,9 @@ import Backbone from 'backbone'
 var HeaderComponent = React.createClass({
 	render: function() {
 		return (
-			<div className="jumbotron"></div>
+			<div className="jumbotron">
+				<p className="pageTitle">Help on the Way</p>
+			</div>
 		)
 	}
 })
