b755c44d57393b1cb697c57e76113ebae65eebe6
james sewell <jamesjosephsewell@gmail.com>
2017-03-11 22:42:41 -0600

    hooked up some components to the homepage

---
+++ src/scripts/views/components/signUpComponent.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Backbone from 'backbone'
+import EmailComponent from './views/components/formComponents/email.js'
+import SubmitComponent from './views/components/formComponents/submit.js'
+import UserNameComponent from './views/components/formComponents/userName.js'
+import PasswordComponent from './views/components/formComponents/password.js'
+
+var SignUpComponent = React.createClass({
+	
+	render: {
+		return(
+			<div className = "SignUpComponent">
+			
+				<EmailComponent />
+				<UserNameComponent />
+				<PasswordComponent />
+				<SubmitComponent />
+				
+			</div>
+		)
+	}
+})
+
+export default SignUpComponent 
