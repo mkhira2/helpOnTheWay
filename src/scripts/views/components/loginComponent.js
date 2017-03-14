81df89b24b50b24ab33decfe5c277b7c9f1f6c80
Patrick Hildreth <pdhildreth@gmail.com>
2017-03-14 16:15:16 -0500

    took out comments and fixed the Headers

--- src/scripts/views/components/loginComponent.js
+++ src/scripts/views/components/loginComponent.js
@@ -4,33 +4,6 @@ import Backbone from 'backbone'
 import ACTIONS from '../../actions.js'
 
 var LoginComponent = React.createClass({
-
-	// _createAction: function(evt) {
-	// 	evt.preventDefault()
-	// 	ACTIONS.createNewUser({
-	// 		email:evt.target.email.value,
-	// 		userName:evt.target.userName.value,
-	// 		password:evt.target.password.value
-	// 	})
-	// },
-	// render: function () {
-	// 	console.log('rendering sign in')
-	// 	return(
-	// 		<div className = "SignUpComponent col-sm-5 offset-sm-1">
-	// 			<form onSubmit={this._createAction} id="register">
-	// 				<h3>Register:</h3>
-	// 				<label>Email Address</label>
-	// 				<input type="text" className="form-control" name="email" placeholder="Enter Email Address" />
-	// 				<label>User Name</label>
-	// 				<input placeholder="Enter User Name" type="text" name="userName" className="form-control" />
-	// 				<label>Password</label>
-	// 				<input type="password" className="form-control" name="password" placeholder="Enter Password" />
-	// 				<button type="submit" form="register" className="btn btn-primary">Submit</button>	
-	// 			</form>
-	// 		</div>
-	// 	)
-	// },
-
 	_createAction: function(evt) {
 		evt.preventDefault()
 		ACTIONS.logUserIn(evt.target.emailLogin.value,evt.target.password.value)
@@ -41,7 +14,7 @@ var LoginComponent = React.createClass({
 
 			<div className="loginComponent col-sm-5 justify-content-center">
 				<form onSubmit={this._createAction}>
-					<h2>Login:</h2>
+					<h3>Login:</h3>
 					<label>User Name</label>
 					<input placeholder="Enter User Name" type="text" name="emailLogin" className="form-control" />
 					<label>Password</label>
