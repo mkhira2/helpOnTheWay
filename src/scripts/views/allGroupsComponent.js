1f4e589e610fd6f1c551cb6c57f7ff46246dfd60
Reggie Evans <REvans911@gmail.com>
2017-03-13 19:25:55 -0500

    added new group page with router and nav element

--- src/scripts/views/allGroupsComponent.js
+++ src/scripts/views/allGroupsComponent.js
@@ -2,8 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Backbone from 'backbone'
 import init from '../init'
-import HeaderComponent from './groupViews/headerComponent'
-import FooterComponent from './groupViews/footerComponent'
+import HeaderComponent from './headerComponent'
+import FooterComponent from './footerComponent'
 
 const AllGroups = React.createClass({
 	render: function (){
@@ -11,22 +11,22 @@ const AllGroups = React.createClass({
 			<div className="container">
 				<HeaderComponent />
 				<div className= "row">
-					<button className="btn btn-primary col-md-5">Create Group</button>
-					<input  name="name" type='text' className='groupNameInput form-control col-md-5' placeholder='Group Name'/>
+					<button className="btn btn-primary col-md-3">Create Group</button>
+					<input  name="name" type='text' className='groupNameInput form-control col-md-8' placeholder='Search group'/>
 				</div>
 				<div className="row">
-					<ul className="list-group">
+					<ul className="list-group col-12">
 						<div>
 						<li className="list-group-item">Group 1</li>
-						<button className="btn btn-primary col-md-5">Create Group</button>
+						<button className="btn btn-success col-md-2">Join Group</button>
 						</div>
 						<div>
 						<li className="list-group-item">Group 2</li>
-						<button className="btn btn-primary col-md-5">Create Group</button>
+						<button className="btn btn-success col-md-2">Join Group</button>
 						</div>
 						<div>
 						<li className="list-group-item">Group 3</li>
-						<button className="btn btn-primary col-md-5">Create Group</button>
+						<button className="btn btn-success col-md-2">Join Group</button>
 						</div>
 					</ul>
 				</div>
